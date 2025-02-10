9bdf9971bc7d1b29a633c6ec6b7bbcdc561ffa21
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-09 20:38:47 -0500

    add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const { DivisionData } = require('./data/divison-data');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
