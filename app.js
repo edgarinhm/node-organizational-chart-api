24167a4ac902f943ac54af3de343dcbfb00a231f
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-10 11:22:16 -0500

    add file functions ops add position routes add body parser

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
+const bodyParser = require('body-parser');
 dotenv.config();
 
 const DivisionData = require('./data/divison-data');
 const corsOptions = require('./config/cors-options');
 
 const PORT = process.env.PORT || 3000;
+const FILES_DIR = 'data';
+const PositionsData = require('./data/positions-data');
+
+const fs = require('fs');
+const { promises: fsPromises } = fs;
+
+const ReadFile = async (fileName, encoding = 'utf8') => {
+    return await fsPromises.readFile(path.join(__dirname, FILES_DIR, fileName), encoding);
+}
+
+const CreateFile = async (fileName, content) => {
+    return await fsPromises.writeFile(path.join(__dirname, FILES_DIR, fileName), content);
+}
+
+const UpdateFile = async (fileName, content) => {
+    return await fsPromises.appendFile(path.join(__dirname, FILES_DIR, fileName), content);
+}
+
+const DeleteFile = async (fileName) => {
+    return await fsPromises.unlink(path.join(__dirname, FILES_DIR, fileName));
+}
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use(cors(corsOptions));
 
@@ -18,6 +44,35 @@ app.get('/division', async (req, res) => {
         res.json(response);
     } catch (error) {
         res.status(500).json({ message: 'Error Getting Divisions' });
+    }
+});
+
+app.get('/positions', async (req, res) => {
+    try {
+        const response = await Promise.resolve({ data: PositionsData });
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ message: 'Error Getting Positions' });
+    }
+});
+
+app.post('/positions', async (req, res) => {
+    try {
+        const newPosition = { id: PositionsData.length + 1, name: req.body.name, division: req.body.division, tier: req.body.tier, parentId: req.body.parentId ?? 0 };
+        PositionsData.push(newPosition);
+        res.send(PositionsData);
+    } catch (error) {
+        res.status(500).json({ message: 'Error Creating Position', error });
+    }
+});
+
+app.put('/positions:id', async (req, res) => {
+    try {
+        const newPosition = { name: req.body.name, division: req.body.division, tier: req.body.tier, parentId: req.body.parentId ?? 0 };
+        PositionsData.push(newPosition);
+        res.send(PositionsData);
+    } catch (error) {
+        res.status(500).json({ message: 'Error Updating Position', error });
     }
 });
 
---
+++ data/org-chart-data.js
@@ -0,0 +1,3 @@
+const OrgCharData = [];
+
+module.exports = OrgCharData
